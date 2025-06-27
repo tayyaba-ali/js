4572376aeaf62b5914232771d86608fc100f47c7
tayyaba-ali <ta6402792@gmail.com>
2025-06-27 13:17:21 +0500

    typesript added

---
+++ typescript/class1/main.js
@@ -0,0 +1,16 @@
+// let language:string ;
+// language = "typescript"
+// language = 45
+// let numbers = 45;
+// numbers = "tayyaba"
+// const fruits = ["apple","mango","grapes"]
+// fruits.push(34)
+// const numbers:number[] = [1,14]
+// explicitly
+// implicitly
+// let isPresent:boolean = false
+// let hello:string;
+// hello = "hello"
+// hello = 11;
+var city = "karachi";
+console.log(city);
---
+++ typescript/class1/main.ts
@@ -0,0 +1,30 @@
+
+
+// let language:string ;
+
+// language = "typescript"
+// language = 45
+
+// let numbers = 45;
+// numbers = "tayyaba"
+
+// const fruits = ["apple","mango","grapes"]
+
+// fruits.push(34)
+
+// const numbers:number[] = [1,14]
+
+
+// explicitly
+// implicitly
+
+
+// let isPresent:boolean = false
+
+// let hello:string;
+
+// hello = "hello"
+// hello = 11;
+
+let city:string = "karachi"
+console.log(city)
